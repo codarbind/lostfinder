78bbc1a26676b9a24ba9768b39302b4081ae09d6
abdwahabi <abdwahab.adekola@yknetworks.com.ng>
2021-03-19 01:56:30 +0100

    auth completed

--- index.js
+++ index.js
@@ -4,115 +4,193 @@ const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
+const ejs = require('ejs');
 
-
-mongoose.connect('mongodb://localhost:27017/userDetails', {useNewUrlParser: true});
+const db = mongoose.connect('mongodb://localhost:27017/userDetails', {useNewUrlParser: true});
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
+firstName: String,
+lastName: String,
 userEmail : String,
 userPassword:String,
+randomIdentifier:Number,
 });
-
 const Model = mongoose.model;
-
-const User = Model('userCredentials',UserSchema);
+const User = Model('usercredentials',UserSchema);
 
 
 const app = express();
-app.use(bodyParser.urlencoded());
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(express.static('html'));
+
 
 app.get('/', (req, res)=> {
-
-	var html = "<html> <br> <h2>to sign up</h2> <form method='post' action='/signup'> \
-	 <input name='signEmail' placeholder='email address' type='email' id='signEmail'> <br>\
-	 <input type='password' placeholder='your password' name='signPassword'> <br><button type='submit' name='signSubmit'>Sign Up</button> </form>\
-	  <h2>To log in</h2> \
-	  <form method='post' action='/login'><input type='email' required placeholder='your email address' name='logEmail' > <br> <input required placeholder='password' type='password' name='logPassword'>\
-	  <br> <input type='submit' name='logIn' value='Log In' > </form><html>";
-	res.send('hello, welcome to my sign up and log in endpoints ' + html);
+	let userEmail = ', you are not logged in.';
+	jwt.verify(req.cookies.jwt,'TOP_SECRET' ,function(err,verifiedJwt){
+	verifiedJwt ? userEmail = verifiedJwt.logEmail : userEmail ;
+	res.redirect('home')
+					})
 });
 
+
 app.post('/login', (req, res)=>{
-
-
 		let logEmail = req.body.logEmail;
 		let logPassword = req.body.logPassword;
-	
-	
-	User.find({'userEmail': logEmail}, 'userPassword', function(err, docs){
-
-
-
+		console.log(`${logEmail} and ${logPassword}`);
+		User.find({'userEmail': logEmail}, 'userPassword', function(err, docs){
 		if (docs[0] === undefined)  {
-
-						 console.log('this is the error while fetching '+ err);
-			
+			console.log('this is the error while fetching '+ err);
 			res.send('no such account found');
-
 			} else {
-
 				let retrievedPass = docs[0].userPassword;
+				if (logPassword === retrievedPass) {
+					const token = jwt.sign(
+											{ userEmail:logEmail }, 
+											'TOP_SECRET',
+											{expiresIn: 5*24*60*60}
+										);
+					res.cookie('jwt', token, {httpOnly: true, maxAge:5*24*60*60*1000});
+					res.render('home',{userObject:logEmail})
+				}else{
+					res.send('wrong details')
+				}
 
-			bcrypt.compare(logPassword, retrievedPass ).then(function(result) {
+			/*bcrypt.compare(logPassword, retrievedPass ).then(function(result) {
 
 				if (result) {
-								res.send('you are logged in')
+
+					const token = jwt.sign(
+											{ logEmail }, 
+											'TOP_SECRET',
+											{expiresIn: 5*24*60*60}
+
+										);
+
+
+					res.cookie('jwt', token, {httpOnly: true, maxAge:5*24*60*60*1000}).json('Successfull, okay done');
+
+					/*jwt.verify(req.cookies.jwt,'TOP_SECRET' ,function(err,verifiedJwt){
+					console.log(verifiedJwt); 
+					})
+
+					if(verifiedJwt.userEmail===logEmail){
+						res.send('you are logged in')
+					} else { 
+						res.send('kindly log in with ', logEmail)
+					}*../
+
 							
 								} else {
 									
 									res.send('wrong password');
 								}
 
-				});
+				}); */
 				
 				
 			
 			}
 	});
-
-
 });
 
+
 app.post('/signup', (req, res)=>{
-
-let userEmail = req.body.signEmail;
-let userPassword = req.body.signPassword;
-
-
+let {firstName, lastName, userEmail} = req.body;
+let userPassword = null;
+console.log(`${userEmail} ${userPassword}`);
 User.find({'userEmail': userEmail}, 'userPassword', function(err, docs){
-
 if(docs[0]) return res.json('user email exist');
-
-bcrypt.hash(userPassword, 10, function(err, hash){
-
-if (err) return console.log('this is the hash error '+ err);
-
-userPassword = hash;
-
+let randomIdentifier = Math.floor(Math.random()*100000);
 const newUser = new User({
 
-	userEmail: userEmail,
-	userPassword: userPassword,
+	firstName,
+	lastName,
+	userEmail,
+	userPassword,
+	randomIdentifier,
 
 });
 
 newUser.save((err, results)=>{
-
 	if (err) return console.log('this is the error ' + err);
-const token = jwt.sign({ userEmail }, 'TOP_SECRET',{
-	expiresIn: 5*24*60*60,
+	console.log('sign up successful, this is your email: Welcome to Lostfinder kindly set your password by visiting this link: localhost:3000/pass/'+ randomIdentifier);
+res.send('sign up successful, this is your email: Welcome to Lostfinder kindly set your password by visiting this link: localhost:3000/pass/'+ randomIdentifier);
+});
+setTimeout(()=>(
+	User.deleteOne({randomIdentifier}, function (err) {
+  if(err) console.log(err);
+  console.log("Successful deletion");
+})),120000);
+});
 });
 
 
-res.cookie('jwt', token, {httpOnly: true, maxAge:5*24*60*60*1000}).json('Successfull, okay done');
-
+app.get('/pass/:pass',(req,res)=>{
+	if(!req.params.pass){
+		res.json('do something with the password');
+	} else {
+	var passCheck = User.find({randomIdentifier:req.params.pass})
+					 .then(details =>{
+						details.length === 1
+						? ( console.log('item found, please confirm if your name is '+ details[0].firstName),
+							res.render('setpassword',{details})
+						  )
+														 																	 	 
+						: (console.log('the link must have expired, please try signin up again'),
+					 		res.json('the link must have expired, please try signin up again')
+					 	  )										 		
+							});
+					}
 });
 
+
+app.post('/pass/setpassword',(req,res)=>{
+const {password, userEmail} = req.body;
+User.updateOne({userEmail}, {"$set":{userPassword:password, randomIdentifier: null}},{upsert:false}, function(err){
+	const token = jwt.sign(
+							{ userEmail }, 
+							'TOP_SECRET',
+							{expiresIn: 5*24*60*60}
+							);
+	let cookJwt = res.cookie('jwt', token, {httpOnly: true, maxAge:5*24*60*60*1000});
+	res.redirect('/home');
+});
 });
 
-});
-});
+
+app.post('/home',(req,res)=>{
+	let userEmail = ', you are not logged in.';
+	jwt.verify(req.cookies.jwt,'TOP_SECRET' ,function(err,verifiedJwt){
+					console.log(verifiedJwt); 
+			verifiedJwt ? userEmail = verifiedJwt.userEmail : userEmail ;		
+	res.render('home',{userObject:userEmail})				
+					})
+})
+
+
+app.get('/home',(req,res)=>{
+	let userEmail = ', you are not logged in.';
+	jwt.verify(req.cookies.jwt,'TOP_SECRET' ,function(err,verifiedJwt){
+					console.log(verifiedJwt); 
+			verifiedJwt ? userEmail = verifiedJwt.userEmail : userEmail ;		
+	res.render('home',{userObject:userEmail})				
+					})
+})
+
+
+app.get('/signout',(req,res)=>{
+	res.cookie('jwt', null);
+	let userEmail = ', you are not logged in.';
+	res.redirect('home')
+})
+
+
+app.get('/auth', (req,res)=>{
+	res.render('auth');
+})
+
 
 app.listen(3000, ()=>{ console.log('working at 3000')});
 
